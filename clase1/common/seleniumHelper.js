a255977a7d43198e4d6a5d55a37190a2316aa88d
Samuel Fernandez <SamuelfernandezRodriguez89@gmail.com>
2023-11-14 18:49:05 +0100

    Primeros pasos con Selenium

---
+++ clase1/common/seleniumHelper.js
@@ -0,0 +1,13 @@
+module.exports =  class SeleniumHelper {
+    constructor(driver){
+        this.driver = driver
+    }
+     async waitDOMReady(){
+        const drv = this.driver
+        await this.driver.wait(function() {
+            return drv.executeScript('return document.readyState').then(function(readyState) {
+              return readyState === 'complete';
+            });
+        });
+    }
+}
